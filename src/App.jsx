8ffbb2158732488c1f647932a3c546ff12173170
RamonaXR <Ramonaelise@gmail.com>
2025-03-08 17:00:50 +0100

    app.jsx with imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
-export default function App() {
-  return <div></div>;
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div id="root" className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <Projects />
+      </main>
+      <Footer />
+    </div>
+  );
 }
+
+export default App;
