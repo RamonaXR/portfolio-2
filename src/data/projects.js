eae246c3a97f825e5fb3cbd8da849b09f376a09d
RamonaXR <Ramonaelise@gmail.com>
2025-03-08 14:40:19 +0100

    projects as const

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+export const PROJECTS = [
+  {
+    title: "React E-Commerce Store (JavaScript Frameworks)",
+    image: "images/jsframeworks.jpg",
+    description:
+      "A responsive e-commerce store offering product exploration, cart management, and checkout. Uses Noroff's API, React, and Tailwind.",
+    website: "https://react-ca-ramona.netlify.app",
+    github: "https://github.com/RamonaXR/react-ca.git",
+  },
+  {
+    title: "Holidaze (Project Exam 2)",
+    image: "images/holidaze.jpg",
+    description:
+      "A venue discovery and booking system with search, detailed views, and user authentication. Powered by Noroff's API, React, and Tailwind.",
+    website: "https://holidaze-ramona-pe2.netlify.app",
+    github: "https://github.com/RamonaXR/projectExam-2.git",
+  },
+  {
+    title: "Biddershop (Semester Project 2)",
+    image: "images/biddershop.jpg",
+    description:
+      "An online auction marketplace for browsing, bidding, and selling items. Leverages Noroff's API with Javascript and Tailwind.",
+    website: "https://biddershop-sp2.netlify.app",
+    github: "https://github.com/RamonaXR/Semester-Project-2.git",
+  },
+];
